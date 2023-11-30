cc82003c53345235b28c470fa1dccca4b18e821d
bynullname <bynullname@gmai>
2023-11-30 21:00:45 +0800

    静态deploy

--- config/index.ts
+++ config/index.ts
@@ -1,10 +1,10 @@
 // ~/config/index.ts
-
-const BASE_IP = '192.168.31.177';
+// 获取当前窗口的主机名（例如 'example.com' 或 '192.168.31.177'）
+const currentHost = window.location.hostname;//'192.168.31.177';
 
 export const deviceConfig = {
-    apiUrl: `http://${BASE_IP}:5000`,
-    wsUrl: `ws://${BASE_IP}:7878`,
+    apiUrl: `http://${currentHost}:5000`,
+    wsUrl: `ws://${currentHost}:7878`,
     apiKey: '',
     // 其他配置...
 };
---
+++ deploy.sh
@@ -0,0 +1,2 @@
+npx nuxi generate          
+scp -r .output/public/* root@192.168.31.177:/var/www/X-Inventory/

9d6f6993a58866a8902f31a3816860e4d1e7bc29
bynullname <bynullname@gmai>
2023-12-08 15:58:11 +0800

    SET ROI

--- components/SetROI.vue
+++ components/SetROI.vue
@@ -3,39 +3,85 @@
       <div class="roi-grid">
         <div>
           <el-text class="label-text" type="primary">Top</el-text>
-          <el-input-number v-model="numTop" :precision="2" :step="0.1" :max="10" />
+          <el-input-number v-model="topScaleLinePercent" :precision="2" :step="0.02" :max="0.8" :min="0.1" />
         </div>
         <div>
-          <el-input-number v-model="numBottom" :precision="2" :step="0.1" :max="10" />
+          <el-input-number v-model="bottomValue" :precision="2" :step="0.02" :max="1" :min="0.6" />
           <el-text class="label-text" type="primary">Bottom</el-text>
 
         </div>
         <div>
           <el-text class="label-text" type="primary">Left</el-text>
-          <el-input-number v-model="numLeft" :precision="2" :step="0.1" :max="10" />
+          <el-input-number v-model="leftScaleLinePercent" :precision="2" :step="0.02" :max="0.49" :min="0.01" />
         </div>
         <div>
-          <el-input-number v-model="numRight" :precision="2" :step="0.1" :max="10" />
+          <el-input-number v-model="rightScaleLinePercent" :precision="2" :step="0.02" :max="0.99" :min="0.51" />
           <el-text class="label-text" type="primary">&ensp;Right&ensp;</el-text>
         </div>
       </div>
       <div class="roi-save">
-        <el-button type="primary" :icon="Edit">保存 ROI</el-button>
+        <el-button type="primary" :icon="Edit" @click="handleSave">保存 ROI</el-button>
       </div>
     </div>
   </template>
   
-  <script setup lang="ts">
-    import { ref } from 'vue'
-    import { Delete, Edit, Search,Download, Share, Upload, Switch} from '@element-plus/icons-vue'
-    import { genFileId } from 'element-plus'
+  <script setup>
+    import { ref,onMounted } from 'vue'
+    import { Edit} from '@element-plus/icons-vue'
     import { useInventoryApi } from '~/composables/useInventoryApi'
     import { deviceConfig } from '~/config/index'; // 引入配置文件
-    const uploadAction = ref(`${deviceConfig.apiUrl}/api/upload_wms_excel`);
-    const numTop = ref(0)
-    const numBottom = ref(0)
-    const numLeft = ref(0)
-    const numRight = ref(0)
+    const { setCameraRoi, fetchCameraRoi } = useInventoryApi();
+
+    const topScaleLinePercent = useState('topScaleLinePercent',()=> 0.3);
+    const leftScaleLinePercent = useState('leftScaleLinePercent',()=> 0.1);
+    const rightScaleLinePercent = useState('rightScaleLinePercent',()=> 0.9);
+    const bottomValue = ref(deviceConfig.camera_roi_bottom || 0.9); // 如果 deviceConfig 中没有相应值，则默认为 0.9
+
+    const handleSave = async () => {
+        // 构建要保存的 ROI 数据
+        const roiData = {
+            left: {
+            top: topScaleLinePercent.value,
+            bottom: bottomValue.value, // 假设 bottom 是基于 top 的补数
+            left: leftScaleLinePercent.value,
+            right: rightScaleLinePercent.value
+            },
+            right: {
+            // 这里假设右侧相机的 ROI 设置与左侧相同，根据实际需要调整
+            top: topScaleLinePercent.value,
+            bottom: bottomValue.value,
+            left: leftScaleLinePercent.value,
+            right: rightScaleLinePercent.value
+            }
+        };
+
+        // 调用设置 ROI 的 API
+        const response = await setCameraRoi(roiData);
+        if (response.success) {
+            console.log('ROI 保存成功');
+            ElMessage({
+                    message: 'ROI 保存成功',
+                    type: 'success',
+            })
+        } else {
+            console.error('保存ROI失败: ' + response.message);
+            ElMessage.error('保存ROI失败: ' + response.message);
+
+        }
+    };
+
+    onMounted(async () => {
+        const response = await fetchCameraRoi();
+        if (response.success) {
+            const { left, right } = response.cameraRoi;
+            topScaleLinePercent.value = left.top;
+            leftScaleLinePercent.value = left.left;
+            rightScaleLinePercent.value = left.right;
+            // 如果右侧相机有不同的设置，您也可以相应地更新它们
+        } else {
+            console.error('加载 ROI 设置失败: ' + response.message);
+        }
+    });
   </script>
 
   <style scoped> 
--- components/VideoMonitor.vue
+++ components/VideoMonitor.vue
@@ -1,77 +1,12 @@
 <template>
   <div class="video-monitor">
     <SingleCamera class="camera" label="左侧相机" dir="left" />
-    <!-- <div class="camera-control">
-        <el-switch
-        class="wide-switch"
-        v-model="syncIndependent"
-        active-text="同步"
-        inactive-text="独立">
-      </el-switch>
-      <el-button type="success" @click="handleSave">保存 ROI</el-button>
-    </div> -->
     <SingleCamera class="camera" label="右侧相机" dir="right"/>
   </div>
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
-import { deviceConfig } from '~/config/index';
 
-import { useInventoryApi } from '~/composables/useInventoryApi'; // 确保这里的路径正确
-const { setCameraRoi, fetchCameraRoi } = useInventoryApi();
-
-const syncIndependent = ref(true)
-const topScaleLinePercent = useState('topScaleLinePercent',()=> 0.3);
-const leftScaleLinePercent = useState('leftScaleLinePercent',()=> 0.1);
-const rightScaleLinePercent = useState('rightScaleLinePercent',()=> 0.9);
-const bottomValue = ref(deviceConfig.camera_roi_bottom || 0.9); // 如果 deviceConfig 中没有相应值，则默认为 0.9
-
-const handleSave = async () => {
-  // 构建要保存的 ROI 数据
-  const roiData = {
-    left: {
-      top: topScaleLinePercent.value,
-      bottom: bottomValue.value, // 假设 bottom 是基于 top 的补数
-      left: leftScaleLinePercent.value,
-      right: rightScaleLinePercent.value
-    },
-    right: {
-      // 这里假设右侧相机的 ROI 设置与左侧相同，根据实际需要调整
-      top: topScaleLinePercent.value,
-      bottom: bottomValue.value,
-      left: leftScaleLinePercent.value,
-      right: rightScaleLinePercent.value
-    }
-  };
-
-  // 调用设置 ROI 的 API
-  const response = await setCameraRoi(roiData);
-  if (response.success) {
-    console.log('ROI 保存成功');
-    ElMessage({
-            message: 'ROI 保存成功',
-            type: 'success',
-    })
-  } else {
-    console.error('保存ROI失败: ' + response.message);
-    ElMessage.error('保存ROI失败: ' + response.message);
-
-  }
-};
-
-onMounted(async () => {
-  const response = await fetchCameraRoi();
-  if (response.success) {
-    const { left, right } = response.cameraRoi;
-    topScaleLinePercent.value = left.top;
-    leftScaleLinePercent.value = left.left;
-    rightScaleLinePercent.value = left.right;
-    // 如果右侧相机有不同的设置，您也可以相应地更新它们
-  } else {
-    console.error('加载 ROI 设置失败: ' + response.message);
-  }
-});
 </script>
 
 <style scoped>
